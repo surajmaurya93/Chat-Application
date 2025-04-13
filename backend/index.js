7bb8fd037749f4c2630e7356145c7d9263c75683
Suraj Maurya <surajmaurya0706@gmail.com>
2025-04-14 02:21:41 +0530

    update

--- backend/index.js
+++ backend/index.js
@@ -7,10 +7,14 @@ import messageRoute from "./routes/messageRoute.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import { app,server } from "./socket/socket.js";
+import path from "path";
+
 dotenv.config({});
 
  
 const PORT = process.env.PORT || 5000;
+
+const _dirname = path.resolve();
 
 // middleware
 app.use(express.urlencoded({extended:true}));
@@ -27,6 +31,10 @@ app.use(cors(corsOption));
 app.use("/api/v1/user",userRoute); 
 app.use("/api/v1/message",messageRoute);
  
+app.use(express.static(path.join(_dirname, "/frontend/dist")));
+app.get("*", (_, res) => {
+    res.sendFile(path.resolve(_dirname, "frontend", "dist", "index.html"));
+})
 
 server.listen(PORT, ()=>{
     connectDB();
